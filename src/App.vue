f5e55912b804c1aa3e5101c6d818be068984d26b
aboytsov <aboytsov@phoenixit.ru>
2021-07-27 18:04:00 +0300

    Toggle

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,15 @@
           <newNote :note="note" @addNote="addNote" />
 
           <!--titlt-->
-
-          <h1>{{ title }}</h1>
-
+          <div class="note-header">
+            <h1>{{ title }}</h1>
+            <div class="icons">
+              <svg :class="{ active: grid}" @click="grid = true" style="cursor: pointer;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
+              <svg :class="{ active:!grid}" @click="grid = false" style="cursor: pointer;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
+            </div>
+          </div>
           <!--note list-->
-          <notes :notes="notes" @remove="removeNote" />
+          <notes :notes="notes" :grid="grid" @remove="removeNote" />
         </div>
       </section>
     </div>
@@ -36,6 +40,7 @@ export default {
     return {
       title: "Notes App",
       message: null,
+      grid:true,
       note: {
         title: "",
         descr: "",
--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,10 +1,10 @@
 <template>
   <!-- note list-->
   <div class="notes">
-    <div class="note" v-for="(note, index) in notes" :key="index">
-      <div class="note-header">
+    <div class="note" :class="{ full: !grid}" v-for="(note, index) in notes" :key="index">
+      <div class="note-header" :class="{ full: !grid}">
         <p>{{ note.title }}</p>
-        <p style="cursor: pointer" @click="removeNote(index)">x</p>
+        <p style="cursor: pointer;" @click="removeNote(index)">x</p>
       </div>
       <div class="note-body">
         <p>{{ note.descr }}</p>
@@ -18,6 +18,10 @@ export default {
   props: {
     notes: {
       type: Array,
+      require: true,
+    },
+    grid: {
+      type: Boolean,
       require: true,
     },
   },
@@ -42,14 +46,47 @@ export default {
   padding: 18px 20px;
   margin-bottom: 20px;
   background-color: #ffffff;
+  transition: all .25s cubic-bezier(.02,.01,.47,1);
+  box-shadow: 0 30px 30px rgba(0,0,0,.02);
+  &:hover {
+    box-shadow: 0 30px 30px rgba(0,0,0,.04);
+    transform: translate(0, -6px);
+    transition-delay: 0s !important;
+  }
+  &.full {
+    width: 100%;
+    text-align: center;
+  }
 }
 .note-header {
   display: flex;
   align-items: center;
   justify-content: space-between;
+  h1 {
+    font-size: 32px;
+  }
   p {
     font-size: 22px;
     color: #402caf;
+  }
+  svg {
+    margin-right: 12px;
+    color: #999999;
+    &.active {
+      color: #402caf;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+  &.full {
+    justify-content: center;
+    p {
+      margin-right: 16px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
   }
 }
 .note-body {
